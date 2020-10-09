0ff5daec32b69ebe2950cc93cc6db6a844d38a49
sunqionggang <913111870@qq.com>
2020-10-09 11:28:39 +0800

    init-yz-talk

---
+++ src/main/java/com/sansyou/yz_talks/YzTalksApplication.java
@@ -0,0 +1,13 @@
+package com.sansyou.yz_talks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzTalksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzTalksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sansyou/yz_talks/YzTalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansyou.yz_talks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzTalksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
